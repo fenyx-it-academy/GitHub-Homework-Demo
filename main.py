74dcfd5477bc89ef183ac78f3a04d9116fb834b1
Saffet Senih Çilesiz <av.saffetcilesiz@gmail.com>
2022-09-30 23:42:41 +0200

    Attempt 1

---
+++ main.py
@@ -0,0 +1,14 @@
+def sum_num(a,b):
+    sum = a + b;
+    return sum;
+number1 = 1
+number2 = 2
+print(sum_num(number1, number2))
+
+
+def minus_num(a,b):
+    minus = a - b;
+    return minus;
+number3 = 5
+number4 = 3
+print(minus_num(number3, number4))
