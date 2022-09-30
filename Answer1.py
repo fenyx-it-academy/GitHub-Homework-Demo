ca1574745b67337086420ffca733ebde49a14082
“Ilter” <retli79@gmail.com>
2022-10-01 01:22:43 +0200

    added two files for answers

---
+++ Answer1.py
@@ -0,0 +1,14 @@
+
+# ### Question 1:
+# Please create a function which will take two integers as parameters and will return the sum of them.
+# eg.: 
+
+# ```Python
+# > my_sum_func(1,2)
+# > 3
+# ```
+def my_sum_func(num1, num2):
+    return num1 + num2
+
+print(my_sum_func(1,2))
+
---
+++ Answer2.py
@@ -0,0 +1,13 @@
+# ### Question 2:
+# Please create a function which will take two integers as parameters and will return the subtraction of them.
+# eg.: 
+
+# ```Python
+# > my_subt_func(5,3)
+# > 2
+# ```
+def my_subt_func(num1, num2):
+    return num1- num2
+
+print(my_subt_func(5,3))
+
