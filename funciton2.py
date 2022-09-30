51c340f551c1e39b06a7857270874985c3dc5085
Mikdad <mikdad.kanbar1@gmail.com>
2022-09-30 22:21:13 +0200

    adding two functions

---
+++ funciton2.py
@@ -0,0 +1,11 @@
+# <!-- ### Question 2:
+# Please create a function which will take two integers as parameters and will return the subtraction of them.
+# eg.: 
+
+# ```Python
+# > my_subt_func(5,3)
+# > 2 -->
+
+def sub( num1,num2):
+  return (num1-num2)
+print ( sub(3,1))  
---
+++ function1.py
@@ -0,0 +1,12 @@
+
+# ### Question 1:
+# Please create a function which will take two integers as parameters and will return the sum of them.
+# eg.: 
+
+# ```Python
+# > my_sum_func(1,2)
+# > 3
+
+def sum_numbers(num1,num2):
+    return ( num1+num2)
+print (sum_numbers(1,3)  ) 
